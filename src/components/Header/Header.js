174ba846bad8dc4fb1842317ba4dadcf47c8299e
Liam Swift <lswift@squiz.net>
2020-09-05 13:23:14 +1000

    First commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <button>Reset</button>
+      <a href="/">Home</a>
+    </div>
+  )
+}
+
+export default Header
